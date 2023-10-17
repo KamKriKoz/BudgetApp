8f63cfc2330c87b6c3f07a921af5cc6e4da8b299
Kamil <kmlkozyra@gmail.com>
2023-10-17 15:14:18 +0200

    Added 'Budget' files

---
+++ Budget.h
@@ -0,0 +1,29 @@
+#ifndef BUDGET_H
+#define BUDGET_H
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+class Budget {
+
+    UserManager userManager;
+
+public:
+
+    Budget (string usersFileName)
+        : userManager(usersFileName) {
+    };
+
+    void userRegistration();
+    void showUsers();
+    void logging();
+    void passwordChange();
+    void loggingOut();
+    void loginMenu();
+    void userMenu();
+    bool whetherUserIsLogged();
+};
+
+#endif // BUDGET_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
+#include <conio.h>
+#include "Budget.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hallo Welt!" << endl;
-    return 0;
+int main() {
+
+    Budget budget("Users.xml");
+    char choose;
+
+    while(1) {
+
+        if (!budget.whetherUserIsLogged()) {
+
+            system("cls");
+            budget.loginMenu();
+            choose = getch();
+
+            switch (choose) {
+
+            case '1':
+                budget.userRegistration();
+                break;
+
+            case '2':
+                budget.logging();
+                break;
+
+            case'3':
+                budget.showUsers();
+                break;
+
+            case '9':
+                exit(0);
+
+            default:
+                cout << endl << "Option does not exist." << endl << endl;
+                system("pause");
+                break;
+            }
+
+        } else {
+
+            system("cls");
+            budget.userMenu();
+            choose = getch();
+
+            switch (choose) {
+
+            case '7':
+                budget.passwordChange();
+                break;
+
+            case '8':
+                budget.loggingOut();
+                break;
+
+            default:
+                cout << endl << "Option does not exist." << endl << endl;
+                system("pause");
+                break;
+            }
+        }
+    }
 }
